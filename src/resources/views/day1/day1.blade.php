7d8d8e21bdedf046c93b4bdf4b0d9628efeb25e8
milic178 <marko.milic178@gmail.com>
2024-12-11 15:24:50 +0100

    - day 1 solution done

--- src/resources/views/day1/day1.blade.php
+++ src/resources/views/day1/day1.blade.php
@@ -1,2 +1,47 @@
-<?php
-echo($test);
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Numbers Table</title>
+    <style>
+        table {
+            width: 50%;
+            border-collapse: collapse;
+            margin: 20px auto;
+        }
+        table, th, td {
+            border: 1px solid black;
+        }
+        th, td {
+            padding: 10px;
+            text-align: center;
+        }
+    </style>
+</head>
+<body>
+<h2 style="text-align: center;">Scroll at end for solution</h2>
+
+<h1 style="text-align: center;">Numbers Table 1</h1>
+<table>
+    <tbody>
+    @foreach ($table1 as $row)
+        <tr>
+                <td>{{ $row }}</td>
+        </tr>
+    @endforeach
+    </tbody>
+</table>
+<h1 style="text-align: center;">Numbers Table 2</h1>
+<table>
+    <tbody>
+    @foreach ($table2 as $row)
+        <tr>
+            <td>{{ $row }}</td>
+        </tr>
+    @endforeach
+    </tbody>
+</table>
+
+<h1 style="text-align: center;">Solution day 1 is total distance of: {{$sumDiff}}</h1>
+
+</body>
+</html>
