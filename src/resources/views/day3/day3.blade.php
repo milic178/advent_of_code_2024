5d882afd191af0cf8119355f58a23302dded01a3
milic178 <marko.milic178@gmail.com>
2024-12-12 11:23:50 +0100

    - work on day3 part one

---
+++ src/resources/views/day3/day3.blade.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Numbers Table</title>
+    <style>
+        table {
+            width: 50%;
+            border-collapse: collapse;
+            margin: 20px auto;
+        }
+        table, th, td {
+            border: 1px solid black;
+        }
+        th, td {
+            padding: 10px;
+            text-align: center;
+        }
+    </style>
+</head>
+<body>
+<h2 style="text-align: center;">Solution day 3 part one, matches found: {{$countMatches}}</h2>
+<h2 style="text-align: center;">Solution day 3 part one, matches sum: {{$sumMatches}}</h2>
+
+
+<div style="text-align: center;" >
+    <a href="https://adventofcode.com/2024/day/3">Link Day 3 assignment</a>
+</div>
+<h2 style="text-align: center;">Searchable string</h2>
+{{$searchableString}}
+</body>
+</html>
--- src/routes/web.php
+++ src/routes/web.php
@@ -6,3 +6,4 @@ Route::get('/', [App\Http\Controllers\HomeController::class, 'index'])->name('ho
 
 Route::get('/day1', [App\Http\Controllers\Day1Controller::class, 'solve']);
 Route::get('/day2', [App\Http\Controllers\Day2Controller::class, 'solve']);
+Route::get('/day3', [App\Http\Controllers\Day3Controller::class, 'solve']);
